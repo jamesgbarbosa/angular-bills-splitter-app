52d578d2e40a15d9f69c26ce9a0087351877feab
james <james.barbosa@gmail.com>
2024-03-04 16:41:21 +0800

    Updated form modal

--- src/components/add-transaction-modal/add-transaction-modal.component.ts
+++ src/components/add-transaction-modal/add-transaction-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import {
   MAT_DIALOG_DATA,
   MatDialogRef
@@ -6,13 +8,32 @@ import {
 @Component({
   selector: 'app-add-transaction-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './add-transaction-modal.component.html',
   styleUrl: './add-transaction-modal.component.scss'
 })
-export class AddTransactionModalComponent {
-  constructor(public dialogRef: MatDialogRef<AddTransactionModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
+export class AddTransactionModalComponent implements OnInit {
+  form: FormGroup | any;
 
+  constructor(public dialogRef: MatDialogRef<AddTransactionModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any, private fb: FormBuilder) {
+    let user1 = data.users.length > 0 ? data.users[0].id : null;
+    this.form = this.fb.group({
+      userId: [user1, Validators.required],
+      name: ['', Validators.required],
+      amountPaid: [0, Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  add() {
+    this.dialogRef.close(this.form.value)
   }
 }
