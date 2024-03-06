bf75d7fc4f381df3ad3729419c967a6d441149f9
james <james.barbosa@gmail.com>
2024-03-06 13:59:37 +0800

    Updated model

--- src/model/expenses.model.ts
+++ src/model/expenses.model.ts
@@ -1,12 +1,12 @@
 import { User } from "./user.model";
 
 export interface Expense {
-    userId: string,
+    userId?: string,
     id: string,
     dateCreated: Date,
-    name: string,
-    paidBy: User,
-    credit: any, // {<userId>, <credit/debit balance>, ... }
+    name?: string,
+    paidBy: User | undefined,
+    credit?: any, // {<userId>, <credit/debit balance>, ... }
     amountPaid: number,
-    transactionType: string
+    transactionType?: string
 }
