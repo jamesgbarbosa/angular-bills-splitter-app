dae65f9ab7c2b00c49f9e526a1ac8745da1b51d3
james <james.barbosa@gmail.com>
2024-03-15 10:02:57 +0800

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ProjectComponent } from '../components/project/project.component';
+import { ProjectDetailComponent } from '../components/project-detail/project-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: ProjectComponent
+    },
+    {
+        path: "project/:id",
+        component: ProjectDetailComponent
+    }
+];
