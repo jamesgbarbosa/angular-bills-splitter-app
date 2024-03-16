ddaefe8329f42e008cfbc3e6981500e813bb3a8b
james <james.barbosa@gmail.com>
2024-03-16 21:23:51 +0800

    Updated project model

--- src/model/project.model.ts
+++ src/model/project.model.ts
@@ -2,6 +2,7 @@ import { Expense } from "./expenses.model";
 import { User } from "./user.model";
 
 export interface Project {
+    projectId?: string,
     projectName?: string,
     dateCreated?: Date,
     users: User[],
