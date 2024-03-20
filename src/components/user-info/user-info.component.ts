8e51b052f872c05ef66e5faf2ecf575d617bec27
james <james.barbosa@gmail.com>
2024-03-20 15:31:34 +0800

    Updated styling

--- src/components/user-info/user-info.component.ts
+++ src/components/user-info/user-info.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbsPipe } from '../../pipes/abs.pipe';
 import { Project } from '../../model/project.model';
+import { User } from '../../model/user.model';
 
 @Component({
   selector: 'app-user-info',
@@ -10,6 +11,14 @@ import { Project } from '../../model/project.model';
   templateUrl: './user-info.component.html',
   styleUrl: './user-info.component.scss'
 })
-export class UserInfoComponent {
+export class UserInfoComponent implements OnInit {
   @Input() project: Project | undefined;
+  users : any = [];
+  ngOnInit(): void {
+    this.users = this.project?.users.map((user: User) => ({...user, isExpanded: false}))
+  }
+  
+  toggleExpand(user: any) {
+    user.isExpanded = !user.isExpanded;
+  }
 }
